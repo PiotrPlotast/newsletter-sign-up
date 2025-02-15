44f572d515df85111b2572951e5b1f02db29b539
Pietrell <piotrplotast2006@gmail.com>
2025-02-15 15:34:15 +0100

    feat: enhance SuccessMessage component styling for better responsiveness and readability for pc

--- newsletter-sign-up/src/SuccessMessage.tsx
+++ newsletter-sign-up/src/SuccessMessage.tsx
@@ -12,20 +12,20 @@ export default function SuccessMessage({
   isSubscribed,
 }: SuccessMessageProps) {
   return (
-    <div className="h-full mx-6 pt-36 pb-12 flex flex-col justify-between">
+    <div className="h-full mx-6 pt-36 pb-12 flex flex-col justify-between min-lg:bg-neutral-white min-lg:max-w-[34rem] min-lg:max-h-fit min-lg:rounded-4xl  min-lg:p-16">
       <div>
         <img src={successIcon} alt="" />
         <h1 className="text-neutral-blue-800 leading-none my-8 text-4xl font-bold min-lg:text-6xl ">
           Thanks for subscribing!
         </h1>
-        <p>
+        <p className="max-w-[32rem] min-lg:max-w-[25rem] text-neutral-blue-800">
           A confirmation email has been sent to{" "}
           <b className="text-neutral-blue-700">{email}</b>. Please open it and
           click the button inside to confirm your subscription.
         </p>
       </div>
       <button
-        className="mt-4 cursor-pointer font-bold bg-neutral-blue-800 py-4 text-white rounded-lg hover:bg-gradient-to-l from-primary to-primary2"
+        className="mt-4 cursor-pointer font-bold bg-neutral-blue-800 py-4 text-white rounded-lg hover:bg-gradient-to-l from-primary to-primary2 min-lg:mt-10"
         onClick={() => setIsSubscribed(!isSubscribed)}
       >
         Dismiss message
