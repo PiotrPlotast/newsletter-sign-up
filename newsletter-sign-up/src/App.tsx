9c4f074824363202979b2863175061b900295f98
Pietrell <piotrplotast2006@gmail.com>
2025-02-15 14:52:47 +0100

    feat: pass email and subscription state to SuccessMessage component

--- newsletter-sign-up/src/App.tsx
+++ newsletter-sign-up/src/App.tsx
@@ -4,15 +4,21 @@ import SuccessMessage from "./SuccessMessage";
 import { useState } from "react";
 function App() {
   const [isSubscribed, setIsSubscribed] = useState(false);
+  const [email, setEmail] = useState("");
   return (
     <>
       <div className="min-lg:bg-neutral-blue-700 w-[100dvw] h-[100dvh] min-lg:flex min-lg:items-center min-lg:justify-center">
         {isSubscribed ? (
-          <SuccessMessage />
+          <SuccessMessage
+            email={email}
+            isSubscribed={isSubscribed}
+            setIsSubscribed={setIsSubscribed}
+          />
         ) : (
           <SignUpForm
             isSubscribed={isSubscribed}
             setIsSubscribed={setIsSubscribed}
+            setEmail={setEmail}
           />
         )}
       </div>
