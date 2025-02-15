414b0edc46565c6731ca2655ed36b0fd7e7623bf
Pietrell <piotrplotast2006@gmail.com>
2025-02-15 12:01:04 +0100

    refactor: improve layout responsiveness in App component

--- newsletter-sign-up/src/App.tsx
+++ newsletter-sign-up/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   const [isSubscribed, setIsSubscribed] = useState(false);
   return (
     <>
-      <div className="min-lg:bg-neutral-blue-700 w-[100dvw] h-[100dvh] flex items-center justify-center">
+      <div className="min-lg:bg-neutral-blue-700 w-[100dvw] h-[100dvh] min-lg:flex min-lg:items-center min-lg:justify-center">
         {isSubscribed ? (
           <SuccessMessage />
         ) : (
